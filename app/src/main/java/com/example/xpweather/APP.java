72be9bc0e01f56660d0ed36796edef21ec444a75
xiao <763485391@qq.com>
2017-08-10 14:43:22 +0800

    add instance

--- app/src/main/java/com/example/xpweather/APP.java
+++ app/src/main/java/com/example/xpweather/APP.java
@@ -1,6 +1,7 @@
 package com.example.xpweather;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.lzy.okgo.OkGo;
 
@@ -11,10 +12,22 @@ import org.litepal.LitePalApplication;
  * Created by 肖磊 on 2017/8/7.
  */
 
-public class APP extends Application{
+public class APP extends Application {
+    private static APP instance;
+
+    // 全局 Context 获取
+    public static Context getAppContext() {
+        return instance.getApplicationContext();
+    }
+
+    public static APP getInstance() {
+        return instance;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        instance = this;
         LitePalApplication.initialize(this);
         OkGo.getInstance().init(this);
     }
